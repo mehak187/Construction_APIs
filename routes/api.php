8b4cc220eb7749a941bd05e4b370248c9e1848ac
mehak-fabtechsol <mehak@fabtechsol.com>
2024-05-25 12:43:48 +0500

    removed extra code

--- routes/api.php
+++ routes/api.php
@@ -37,37 +37,3 @@ Route::controller(AuthController::class)->group(function(){
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
 });
-
-Route::controller(ProductController::class)->middleware(['auth:sanctum'])->group(function(){
-    Route::post('store/product', 'storeProduct');
-    Route::post('update/product', 'updateProduct');
-    Route::post('delete/product/{id}', 'deleteProduct');
-    Route::get('products', 'products');
-    Route::get('single/product/{id}', 'singleProduct');
-    Route::post('vote/product', 'voteProduct');
-
-    Route::post('add/promotion', 'addPromotion');
-    Route::get('get/promotion', 'getPromotion');
-});
-Route::controller(MerchantController::class)->middleware(['auth:sanctum'])->group(function(){
-    Route::post('store/merchant', 'storeMerchant');
-});
-
-Route::controller(PaymentController::class)->middleware(['auth:sanctum'])->group(function(){
-    Route::post('/payment', 'processPayment');
-    Route::get('/purchased/history', 'purchasedHistory');
-    Route::get('/single/purchased/{id}', 'singlePurchased');
-});
-
-Route::controller(ProfileController::class)->middleware(['auth:sanctum'])->group(function(){
-    Route::get('/getProfile', 'getProfile');
-    Route::post('/updateProfile', 'updateProfile');
-    Route::post('/changeProfile', 'changeProfile');
-    Route::get('/getData', 'getData');
-
-});
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/notifications', [NotificationController::class, 'getNotification']);
-    Route::get('/markNotificationAsRead', [NotificationController::class, 'markNotificationAsRead']);
-});
